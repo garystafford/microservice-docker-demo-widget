1f5692f49166d0c2fd09e288d55ce7ef16d42787
Gary Stafford <garystafford@rochester.rr.com>
2016-08-16 22:54:53 -0400

    Changes to project...

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![https://travis-ci.org/garystafford/microservice-docker-demo-widget.svg?branch=master](https://travis-ci.org/garystafford/microservice-docker-demo-widget.svg?branch=master)
+
 # microservice-docker-demo-widget
 
 #### Introduction
--- deploy.sh
+++ deploy.sh
@@ -27,4 +27,4 @@ git commit -m "Deploy Travis CI build #${TRAVIS_BUILD_NUMBER} artifacts to GitHu
 # export GH_TOKEN=721fd28bc24dedf553c9ecb6e6c2f137fffb8b5f
 # travis encrypt GH_TOKEN=721fd28bc24dedf553c9ecb6e6c2f137fffb8b5f --add
 # export GH_REF=github.com/garystafford/microservice-docker-demo-artifacts.git
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:build-artifacts > /dev/null 2>&1
