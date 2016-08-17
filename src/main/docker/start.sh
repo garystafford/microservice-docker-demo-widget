9ab509aba713ca29f12281087112620f9cb5d4fa
Gary Stafford <garystafford@rochester.rr.com>
2016-08-17 19:24:41 -0400

    Fixing filebeat

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -2,3 +2,5 @@
 
 curl -XPUT 'http://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
 filebeat -c /etc/filebeat/filebeat.yml -e
+
+java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
