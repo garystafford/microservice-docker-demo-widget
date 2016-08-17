495633b4a34a54e83b4289a87a71bbd89ce8639b
Gary Stafford <garystafford@rochester.rr.com>
2016-08-17 13:51:51 -0400

    Fixing filebeat configuration

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 curl -XPUT 'http://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
-/etc/init.d/filebeat start
+filebeat -c /etc/filebeat/filebeat.yml
