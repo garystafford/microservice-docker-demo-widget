405e741d6141b68596b66fff5673a29897344064
Gary Stafford <garystafford@rochester.rr.com>
2016-08-28 18:04:27 -0400

    Fixing filebeat

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# hack because elk was resolving to wrong IP of 198.105.244.228?
+echo $(getent hosts elk) >> /etc/hosts
+
 curl -XPUT 'http://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
 filebeat -c /etc/filebeat/filebeat.yml -e &
 
