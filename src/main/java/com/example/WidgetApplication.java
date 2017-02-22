293c7fc6e4993f202662e67ef504a749a7a24ca1
Gary Stafford <garystafford@rochester.rr.com>
2017-02-21 22:14:27 -0500

    Refacoring for consul

--- src/main/java/com/example/WidgetApplication.java
+++ src/main/java/com/example/WidgetApplication.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class WidgetApplication {
 
     public static void main(String[] args) {
